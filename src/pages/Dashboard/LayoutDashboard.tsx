522bfdd76d2da93f41959deaebea32a8975b7cbb
Domingos Nkula Pedro <2001.dnp@gmail.com>
2025-07-03 12:11:12 +0100

    update

--- src/pages/Dashboard/LayoutDashboard.tsx
+++ src/pages/Dashboard/LayoutDashboard.tsx
@@ -1,20 +1,22 @@
 import { Outlet } from "react-router-dom";
 import style1 from "../../styles/layoutDashboard.module.css";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import NavBar from "./Components/NavBar";
+import { useState } from "react";
 
 
 const Home = () => {
 
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
 
     return (
         <div className={style1.dashWrap}>
             <header className={style1.dashHeader}>
                 
                 <div className={style1.menuLogo}>
-                    <div className={style1.boxLogoMenu}>
-                        <Menu />
-                    </div>
+                    <button className={style1.boxLogoMenu} onClick={()=> setMenuIsOpen(!menuIsOpen)}>
+                        {menuIsOpen ? <Menu /> : <X />}
+                    </button>
 
                     <div className={style1.boxLogoHeader}>
                         <img src="/icon/quizLogo2.png" />
