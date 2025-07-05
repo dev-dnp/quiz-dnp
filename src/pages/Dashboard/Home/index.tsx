06393e513148e935e1ceb46f3169030d6b384415
Domingos Nkula Pedro <2001.dnp@gmail.com>
2025-07-05 23:05:41 +0100

    Updating

--- src/pages/Dashboard/Home/index.tsx
+++ src/pages/Dashboard/Home/index.tsx
@@ -4,94 +4,96 @@ import clsx from "clsx";
 
 const Home = () => {
   return (
-    <div>
-      <div className={style.primarySeactionCard}>
-          <div className={style.boxProfileHome}>
-            <div className={style.profilePhoto}>
-                <img src="/dnpPhoto.webp" alt="Perfil" width={32}/>
-            </div>
-
-            <div className={style.nameEmailProfile}>
-              <h2 >Domingos Nkula Pedro</h2>
-              <span>domingosnkulajw@gmail.com</span>
-            </div>
-          </div>
-
-          <div className={style.boxCard}>
-              <div className={style.boxIconCard}>
-                  <Flame absoluteStrokeWidth />
+    <div className={style.WrapHomeDash}>
+      <div className={style.mainContentHome}>
+        <div className={style.primarySeactionCard}>
+            <div className={style.boxProfileHome}>
+              <div className={style.profilePhoto}>
+                  <img src="/dnpPhoto.webp" alt="Perfil" width={32}/>
               </div>
 
-              <div className={style.cardDescription}>
-                <h2 className={style.titleDescription}>75862</h2>
-                <span>Pontos acumulados</span>
+              <div className={style.nameEmailProfile}>
+                <h2 >Domingos Nkula Pedro</h2>
+                <span>domingosnkulajw@gmail.com</span>
               </div>
+            </div>
+
+            <div className={style.boxCard}>
+                <div className={style.boxIconCard}>
+                    <Flame absoluteStrokeWidth />
+                </div>
+
+                <div className={style.cardDescription}>
+                  <h2 className={style.titleDescription}>75862</h2>
+                  <span>Pontos acumulados</span>
+                </div>
+            </div>
+        </div>
+
+        {/* SECTION */}
+
+        <div>
+          <div className={style.divTable}>
+            <table className={style.tableBox}>
+              
+              <thead>
+                <tr>
+                <th>ID</th>
+                <th>Pontuação</th>
+                <th>Data</th>
+                <th>Hora</th>
+              </tr>
+              </thead>
+              
+              <tbody>
+
+                <tr>
+                  <td>125</td>
+                  <td className={clsx(style.cellDouble)}>
+                    <TrendingUp className={style.iconColorSuccess}/>
+                    <span>1.250</span>
+                  </td>
+                  <td>
+                    04.07.2025
+                  </td>
+                  <td>
+                    06:14 am
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>125</td>
+                  <td className={clsx(style.cellDouble)}>
+                    <TrendingUp className={style.iconColorWarnning}/>
+                    <span>1.250</span>
+                  </td>
+                  <td>
+                    04.07.2025
+                  </td>
+                  <td>
+                    06:14 am
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>125</td>
+                  <td className={clsx(style.cellDouble)}>
+                    <TrendingDown className={style.iconColorError}/>
+                    <span>1.250</span>
+                  </td>
+                  <td>
+                    04.07.2025
+                  </td>
+                  <td>
+                    06:14 am
+                  </td>
+                </tr>
+
+
+
+              </tbody>
+            </table>
           </div>
-      </div>
-
-      {/* SECTION */}
-
-      <div>
-        <div className={style.divTable}>
-          <table className={style.tableBox}>
-            
-            <thead>
-              <tr>
-              <th>ID</th>
-              <th>Pontuação</th>
-              <th>Data</th>
-              <th>Hora</th>
-            </tr>
-            </thead>
-            
-            <tbody>
-
-              <tr>
-                <td>125</td>
-                <td className={clsx(style.cellDouble)}>
-                  <TrendingUp className={style.iconColorSuccess}/>
-                  <span>1.250</span>
-                </td>
-                <td>
-                  04.07.2025
-                </td>
-                <td>
-                  06:14 am
-                </td>
-              </tr>
-
-              <tr>
-                <td>125</td>
-                <td className={clsx(style.cellDouble)}>
-                  <TrendingUp className={style.iconColorWarnning}/>
-                  <span>1.250</span>
-                </td>
-                <td>
-                  04.07.2025
-                </td>
-                <td>
-                  06:14 am
-                </td>
-              </tr>
-
-              <tr>
-                <td>125</td>
-                <td className={clsx(style.cellDouble)}>
-                  <TrendingDown className={style.iconColorError}/>
-                  <span>1.250</span>
-                </td>
-                <td>
-                  04.07.2025
-                </td>
-                <td>
-                  06:14 am
-                </td>
-              </tr>
-
-
-
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
