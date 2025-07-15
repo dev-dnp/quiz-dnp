59784790ae78cd2fa49360ad131548c27ddb4f77
Domingos Nkula Pedro <2001.dnp@gmail.com>
2025-07-15 23:12:50 +0100

    progress

--- src/pages/Dashboard/StartGame/index.tsx
+++ src/pages/Dashboard/StartGame/index.tsx
@@ -3,7 +3,12 @@ import style from "./style.module.css"
 const StartGame = () => {
   return (
     <div>
-      <div className={style.boxGame}></div>
+      <div className={style.boxGame}>
+        <div className={style.stepGame}></div>
+        <div className={style.askGame}>Você trabalha atualmente como dev?</div>
+        <div className={style.stepGame}></div>
+        <div className={style.stepGame}></div>
+      </div>
     </div>
   )
 }
